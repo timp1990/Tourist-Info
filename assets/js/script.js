b968e491ad19f9c25a219d7cd612fe9f241a6489
Tim Polo <timothyjpolo@gmail.com>
2022-03-31 20:01:22 +1100

    Display Google Map From API

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,13 +9,37 @@ var mapDiv = document.getElementById('map-div');
 var wikiDiv = document.getElementById('wiki-div');
 
 // Declare Global Variables ---------------------------------
+var googleMapsAPIKey = 'AIzaSyAfRxs-L0IXBlCuWGVDzPZKwU8DNotcNTQ';
+var defaultCityCoords = { lat: -25.344, lng: 131.036 };
 
 // ----------------------------------------------------------
 
 // Tim ------------------------------------------------------------
-function googleAPI() {
+function googleAPI(event) {
+    event.preventDefault();
+    console.log(searchInput.value);
+    // Get the latitude and longitude of the searchCity
+
+
     return;
 }
+function initMap() {
+    // The location of thisCity
+    var thisCity = defaultCityCoords;
+    // The map, centered at thisCity
+    var map = new google.maps.Map(mapDiv, {
+        zoom: 4,
+        center: thisCity,
+    });
+    // The marker, positioned at thisCity
+    var marker = new google.maps.Marker({
+        position: thisCity,
+        map: map,
+    });
+}
+
+searchButton.addEventListener('click', googleAPI);
+
 // -------------------------------------------------------------
 
 // Rob ------------------------------------------------------------
@@ -37,3 +61,4 @@ function init() {
 }
 
 init();
+
