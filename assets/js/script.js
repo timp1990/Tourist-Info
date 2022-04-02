c6bcaed5bb48e97fdb758d3c391186fbc6795486
Tim Polo <timothyjpolo@gmail.com>
2022-04-02 14:54:47 +1100

    Add Google Map Interactivity

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,25 +9,29 @@ var mapDiv = document.getElementById('map-div');
 var wikiDiv = document.getElementById('wiki-div');
 
 // Declare Global Variables ---------------------------------
-var googleMapsAPIKey = 'AIzaSyAfRxs-L0IXBlCuWGVDzPZKwU8DNotcNTQ';
+var googleMapsAPIKey = config.mapsKey;
+var googleGeocodingAPIKey = config.geocodingKey;
 var defaultCityCoords = { lat: -25.344, lng: 131.036 };
+var map;
+var cityGoogleObject = {}; // Object has .lat, .long , .address and .touristAttractionsSearchURL variables
+var searchCity = '';
 
 // ----------------------------------------------------------
 
 // Tim ------------------------------------------------------------
 function googleAPI(event) {
     event.preventDefault();
-    console.log(searchInput.value);
     // Get the latitude and longitude of the searchCity
-
-
-    return;
+    searchCity = searchInput.value;
+    codeAddress(searchCity);
+    var GoogleObject = cityGoogleInfo(searchCity);
 }
+
 function initMap() {
     // The location of thisCity
     var thisCity = defaultCityCoords;
     // The map, centered at thisCity
-    var map = new google.maps.Map(mapDiv, {
+    map = new google.maps.Map(mapDiv, {
         zoom: 4,
         center: thisCity,
     });
@@ -36,8 +40,59 @@ function initMap() {
         position: thisCity,
         map: map,
     });
+
+
+    // Let user get lat and long by double click on map
+    map.addListener("dblclick", (mapsMouseEvent) => {
+        // Create a new InfoWindow.
+        infoWindow = new google.maps.InfoWindow({
+            position: mapsMouseEvent.latLng,
+        });
+        infoWindow.setContent(
+            JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2)
+        );
+        infoWindow.open(map);
+    });
 }
 
+function codeAddress(cityForGeocode) {
+    var address = cityForGeocode;
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode({ 'address': address }, function (results, status) {
+        if (status == 'OK') {
+            map.zoom = 12;
+            map.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+                map: map,
+                position: results[0].geometry.location
+            });
+
+        } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+        }
+    });
+}
+
+function cityGoogleInfo() {
+    // https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY
+    // key=AIzaSyA34XiPx9LwWaNldbRx3On1mZH8fm1loZQ
+    var latLongAPIRequest = "https://maps.googleapis.com/maps/api/geocode/json?address=" + searchCity + '&key=' + googleGeocodingAPIKey;
+
+    fetch(latLongAPIRequest)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            // This is where the data response is --- console.log(data)
+            cityGoogleObject.lat = data.results[0].geometry.location.lat;
+            cityGoogleObject.long = data.results[0].geometry.location.lng;
+            cityGoogleObject.address = data.results[0].formatted_address;
+            // https://www.google.com/search?q=places+to+visit+sydney&oq=places+to+visit+sydney&aqs=chrome.0.0i512l5j0i22i30l5.4916j0j7&sourceid=chrome&ie=UTF-8
+            cityGoogleObject.touristAttractionsSearchURL = 'https://www.google.com/search?q=places+to+visit+sydney&oq=places+to+visit+' + searchCity + '&aqs=chrome.0.0i512l5j0i22i30l5.4916j0j7&sourceid=chrome&ie=UTF-8';
+            console.log(cityGoogleObject.touristAttractionsSearchURL);
+        });
+
+}
 searchButton.addEventListener('click', googleAPI);
 
 // -------------------------------------------------------------
